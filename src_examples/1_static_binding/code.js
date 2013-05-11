3898b5fa57c71fff58124a0d1e72bf8593d0dd11
Eugene Guryanov <guryanovev@gmail.com>
2013-05-11 18:32:06 +0400

    Added: default travis config

---
+++ src_examples/1_static_binding/code.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Eugene Guryanov
+ * Date: 11.05.13
+ * Time: 16:34
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ test_api/3_listBinding.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Eugene Guryanov
+ * Date: 10.05.13
+ * Time: 13:07
+ * To change this template use File | Settings | File Templates.
+ */
