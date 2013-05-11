bf448a6ca411b3a649b51eb2f60aff2f9f88cff9
Eugene Guryanov <guryanovev@gmail.com>
2013-05-11 20:31:04 +0400

    Added: new examples structure

--- scripts/build_tutorial.js
+++ scripts/build_tutorial.js
@@ -1,7 +1,62 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Eugene Guryanov
- * Date: 11.05.13
- * Time: 16:24
- * To change this template use File | Settings | File Templates.
- */
+var fs = require('fs');
+var path = require('path');
+//var ent = require('ent');
+
+var examplesBaseDir = "src_examples";
+var outputHtmlFile = path.join("out", "examples.html");
+
+console.log("Building examples html...");
+
+var exampleDirs = fs.readdirSync(examplesBaseDir);
+var examplesCount = exampleDirs.length;
+
+console.log(examplesCount + " examples found");
+
+var examplesFileHeader =
+"<!DOCTYPE html>" +
+"<head></head>" +
+"<html>" +
+"   <body>";
+
+var examplesFileFooter =
+"   </body>" +
+"</html>";
+
+
+fs.writeFileSync(outputHtmlFile, "");
+fs.appendFileSync(outputHtmlFile, examplesFileHeader);
+
+for (var i = 0; i < examplesCount; i++){
+    var exampleId = exampleDirs[i];
+    var exampleDir = path.join(examplesBaseDir, exampleId);
+    var contentFile = path.join(exampleDir, "content.html");
+    var codeFile = path.join(exampleDir, "code.js");
+    var markupFile = path.join(exampleDir, "markup.html");
+
+    if (!fs.existsSync(contentFile)){
+        throw new Error("Required file " + contentFile + " not found!");
+    }
+
+    console.log("Building example " + exampleId);
+
+    fs.appendFileSync(outputHtmlFile, "<div id='" + exampleId + "' class='example'>");
+    fs.appendFileSync(outputHtmlFile, fs.readFileSync(contentFile));
+
+    fs.appendFileSync(outputHtmlFile, "<h3>HTML:</h3>");
+    fs.appendFileSync(outputHtmlFile, "<section class='html'>");
+    fs.appendFileSync(outputHtmlFile, "<pre><code class='html'>");
+    //fs.appendFileSync(outputHtmlFile, ent.encode(fs.readFileSync(markupFile).toString()));
+    fs.appendFileSync(outputHtmlFile, "</code></pre>");
+    fs.appendFileSync(outputHtmlFile, "</section>");
+
+    fs.appendFileSync(outputHtmlFile, "<h3>JavaScript:</h3>");
+    fs.appendFileSync(outputHtmlFile, "<section class='javascript'>");
+    fs.appendFileSync(outputHtmlFile, "</section>");
+
+    fs.appendFileSync(outputHtmlFile, "<h3>Result:</h3>");
+    fs.appendFileSync(outputHtmlFile, "<section class='result'>");
+    fs.appendFileSync(outputHtmlFile, "</section>");
+    fs.appendFileSync(outputHtmlFile, "</div>");
+}
+
+fs.appendFileSync(outputHtmlFile, examplesFileFooter);
--- src_examples/1_static_binding/code.js
+++ src_examples/1_static_binding/code.js
@@ -1,7 +1,1 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Eugene Guryanov
- * Date: 11.05.13
- * Time: 16:34
- * To change this template use File | Settings | File Templates.
- */
+js.bind("John Smith").to("#binding_basicUsage_staticValue .fullName");
