4ff5c77d245f2bbcc1c9ee84c62e831c7fac67c3
Eugene Guryanov <guryanovev@gmail.com>
2013-11-07 14:19:45 +0400

    Added: dependent value

--- src/binding/DependentValue.ts
+++ src/binding/DependentValue.ts
@@ -1,3 +1,77 @@
-/**
- * Created by Eugene Guryanov on 07.11.13.
- */
+/// <reference path="Contracts.ts"/>
+
+module JohnSmith.Binding {
+    export class DependentValue implements IBindable {
+        private _listeners: JohnSmith.Common.ArrayList;
+        private _evaluateValue: () => any;
+        private _dependencies: IBindable[];
+
+        constructor(evaluate: () => any, ...dependencies: IBindable[]) {
+
+            var dependentValue = this;
+            for (var i = 0; i < dependencies.length; i++) {
+                var dependency = dependencies[i];
+                dependency.addListener({
+                    valueChanged: function(oldValue: Object, newValue: Object, changeType: DataChangeReason) {
+                        dependentValue.notifyListeners();
+                    }
+                });
+            }
+
+            this._dependencies = dependencies;
+            this._evaluateValue = evaluate;
+            this._listeners = new JohnSmith.Common.ArrayList();
+        }
+
+        public getValue():any {
+            var actualDependencies: any[] = [];
+            console.log(this._dependencies.length);
+            for (var i = 0; i < this._dependencies.length; i++) {
+                console.log(i + " = " + this._dependencies[i].getValue());
+                actualDependencies.push(this._dependencies[i].getValue());
+            }
+
+            return this._evaluateValue.apply(this, actualDependencies);
+        }
+
+        public setValue(value: any) {
+            throw Error("Could not set dependent value");
+        }
+
+        public getState() {
+            return "";
+        }
+
+        // todo remove
+        public setState(state: string) {
+        }
+
+        public addListener(listener: IBindableListener) {
+            this._listeners.add(listener);
+        }
+
+        public removeListener(listener: IBindableListener) {
+            var indexToRemove: number = -1;
+            for (var i = 0; i < this._listeners.count(); i++) {
+                if (this._listeners.getAt(i) == listener) {
+                    indexToRemove = i;
+                }
+            }
+
+            if (indexToRemove >= 0) {
+                this._listeners.removeAt(indexToRemove);
+            }
+        }
+
+        public getListenersCount(): number {
+            return this._listeners.count();
+        }
+
+        public notifyListeners(): void {
+            for (var i = 0; i < this._listeners.count(); i++) {
+                var listener: IBindableListener = this._listeners.getAt(i);
+                listener.valueChanged(null, this.getValue(), DataChangeReason.replace);
+            }
+        }
+    }
+}
--- test/unit/binding/DependentValueTests.js
+++ test/unit/binding/DependentValueTests.js
@@ -1,3 +1,37 @@
-/**
- * Created by Eugene Guryanov on 07.11.13.
- */
+var testCase = TestCase("unit.binding.DependentValueTests");
+
+testCase.prototype.testCallsListenerOnDependenciesChange = function() {
+    var firstName = new JohnSmith.Binding.BindableValue();
+    var lastName = new JohnSmith.Binding.BindableValue();
+    var fullName = new JohnSmith.Binding.DependentValue(
+        function(){ return null; },
+        firstName,
+        lastName);
+
+    var changeSpy = sinon.spy();
+    fullName.addListener({
+        valueChanged: changeSpy
+    });
+
+    firstName.setValue("John");
+    assertEquals("Change called", 1, changeSpy.callCount);
+
+    lastName.setValue("Smith");
+    assertEquals("Change called", 2, changeSpy.callCount);
+};
+
+testCase.prototype.test_getValue_shouldReturnEvaluatedValue = function() {
+    var firstName = new JohnSmith.Binding.BindableValue();
+    var lastName = new JohnSmith.Binding.BindableValue();
+    var fullName = new JohnSmith.Binding.DependentValue(
+        function(firstNameValue, lastNameValue){
+            return firstNameValue + " " + lastNameValue;
+        },
+        firstName,
+        lastName);
+
+    firstName.setValue("John");
+    lastName.setValue("Smith");
+
+    assertEquals("Evaluated value", "John Smith", fullName.getValue());
+};
