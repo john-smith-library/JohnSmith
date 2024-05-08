bdf6b6781c9ceabfce29e07a94ccb799d13f5ea7
Eugene <eguryanov@codereign.net>
2024-05-08 13:43:03 +0300

    included docs sample to lint files set

--- src_docs/view/hooks-onbeforeinit.tsx
+++ src_docs/view/hooks-onbeforeinit.tsx
@@ -1,34 +1,36 @@
-import {DomElement, HtmlDefinition, View} from '../../src/view';
-import {OnBeforeInit} from "../../src/view/hooks";
-import {DomEngine} from "../../src/view/dom-engine";
-import {OptionalDisposables} from "../../src/common";
-import {ObservableValue} from "../../src/reactive";
+import { DomElement, HtmlDefinition, View } from 'john-smith/view';
+import { OnBeforeInit } from 'john-smith/view/hooks';
+import { DomEngine } from 'john-smith/view/dom-engine';
+import { OptionalDisposables } from 'john-smith/common';
+import { ObservableValue } from 'john-smith/reactive';
 
 export class User {
-    firstName = new ObservableValue();
+  firstName = new ObservableValue<string>('');
 }
 
 export class UserView implements View, OnBeforeInit {
-    constructor(
-        private viewModel: User) {
-    }
+  constructor(private viewModel: User) {}
 
-    template(): HtmlDefinition {
-        return (<div>User</div>);
-    }
+  public template(): HtmlDefinition {
+    return <div>User</div>;
+  }
 
-    onBeforeInit(destination: DomElement, domEngine: DomEngine): OptionalDisposables {
-        // we can create/change DOM manually here
-        const title = domEngine.createElement('h1');
-        destination.appendChild(title);
+  public onBeforeInit(
+    destination: DomElement,
+    root: DomElement | null,
+    domEngine: DomEngine
+  ): OptionalDisposables {
+    // we can create/change DOM manually here
+    const title = domEngine.createElement('h1');
+    destination.appendChild(title);
 
-        // If we create any Disposables we should return them as a result
-        // so that they will be released once the View is unrendered.
-        return this.viewModel.firstName.listen(firstName => {
-            // my own listener logic goes here
+    // If we create any Disposables we should return them as a result
+    // so that they will be released once the View is unrendered.
+    return this.viewModel.firstName.listen(firstName => {
+      // my own listener logic goes here
 
-            // a demo of manual reacting on View Model changes:
-            title.setInnerText('My name is: ' + firstName);
-        });
-    }
+      // a demo of manual reacting on View Model changes:
+      title.setInnerText('My name is: ' + firstName);
+    });
+  }
 }
--- src_docs/view/view-definition-view-constructor.tsx
+++ src_docs/view/view-definition-view-constructor.tsx
@@ -3,11 +3,9 @@ import { HtmlDefinition, View } from '../../src/view';
 export class User {} // this is a View Model
 
 export class UserView implements View {
-    constructor(
-        private viewModel: User) {
-    }
+  constructor(private viewModel: User) {}
 
-    template(): HtmlDefinition {
-        return (<div>User</div>);
-    }
+  public template(): HtmlDefinition {
+    return <div>User</div>;
+  }
 }
